33056dcd2a6ad5a1d901f3fea7ae523f7f50e08d
tetradev01 <tetradev01@tetratutorials.com>
2017-01-19 23:46:08 +0530

    Update index file

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App For Tetranoodle</h1>
+                <h1>Testing Github Webhook for automatic build</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
