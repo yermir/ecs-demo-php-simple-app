b2c9bd85376fdceeb9ad0d29ae6a8b3378a2cde6
yermir <yermir.rivero@gmail.com>
2018-06-30 21:10:16 +0200

    change

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Testing Github Webhook for automatic build YERMIR</h1>
+                <h1>Testing Github Webhook for automatic build YERMIR R</h1>
                 <h2>Congratulations YERMIR FUNCIONA SI</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
